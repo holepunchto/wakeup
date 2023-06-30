e3fcbe7bba41a130ad701436f0aff76cedbd5205
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-06-30 10:43:50 +0200

    Use constructor function for creating array

--- src/darwin/launch.m
+++ src/darwin/launch.m
@@ -5,7 +5,7 @@
 
 int
 wakeup_launch (const wakeup_app_t *app, size_t argc, const char *argv[]) {
-  NSMutableArray<NSString *> *arguments = [[NSMutableArray alloc] initWithCapacity:argc];
+  NSMutableArray<NSString *> *arguments = [NSMutableArray arrayWithCapacity:argc];
 
   for (size_t i = 0; i < argc; i++) {
     arguments[i] = [NSString stringWithFormat:@"%s", argv[i]];
@@ -16,8 +16,6 @@ wakeup_launch (const wakeup_app_t *app, size_t argc, const char *argv[]) {
   NSWorkspaceOpenConfiguration *configuration = [NSWorkspaceOpenConfiguration configuration];
 
   configuration.arguments = arguments;
-
-  [arguments release];
 
   [[NSWorkspace sharedWorkspace]
     openApplicationAtURL:url
