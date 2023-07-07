dacfce645d2d8a4b480c4ec9927f0647ea242c31
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-07-07 10:14:31 +0200

    Rename app bundle to Pear and look for `.app` path on macOS

--- src/darwin/launch.m
+++ src/darwin/launch.m
@@ -11,6 +11,20 @@ wakeup_launch (const appling_app_t *app, const char *url) {
   @autoreleasepool {
     NSURL *path = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%s", app->path]];
 
+    NSMutableArray<NSString *> *components = [NSMutableArray arrayWithArray:path.pathComponents];
+
+    while (components.count > 0) {
+      NSString *component = [components lastObject];
+
+      if ([component.pathExtension isEqualToString:@"app"]) break;
+
+      [components removeLastObject];
+    }
+
+    if (components.count == 0) return -1;
+
+    path = [NSURL fileURLWithPathComponents:components];
+
     NSWorkspaceOpenConfiguration *configuration = [NSWorkspaceOpenConfiguration configuration];
 
     configuration.arguments = @[ [NSString stringWithFormat:@"%s", url] ];
