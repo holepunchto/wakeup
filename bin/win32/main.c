a4353a964a29f2c56040966cbc8ca813d67c9c6b
Kasper Isager <kasperisager@hey.com>
2023-08-04 10:26:13 +0200

    Launch applings using `ShellExecute()` on Windows

--- bin/win32/main.c
+++ bin/win32/main.c
@@ -4,7 +4,7 @@ int
 main (int argc, char *argv[]) {
   if (argc < 1) return 1;
 
-  int err = wakeup(argv[0]);
+  int err = wakeup(argv[1]);
 
   return err < 0 ? 1 : 0;
 }
--- src/win32/launch.c
+++ src/win32/launch.c
@@ -1,9 +1,12 @@
 #include <appling.h>
+#include <shellapi.h>
 
 #include "../../include/wakeup.h"
 #include "../platform.h"
 
 int
 wakeup_launch (const appling_app_t *app, const char *url) {
-  return -1;
+  INT_PTR res = (INT_PTR) ShellExecute(NULL, "open", app->path, url, NULL, 0);
+
+  return res == 32 ? 0 : -1;
 }
