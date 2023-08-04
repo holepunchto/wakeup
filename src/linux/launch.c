67d80209591ce85d578dd4fb2794754380315be0
Parallels <kasperisager@hey.com>
2023-08-04 11:49:03 +0200

    Launch applings with `exec()` on Linux

--- src/linux/launch.c
+++ src/linux/launch.c
@@ -1,9 +1,17 @@
 #include <appling.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "../../include/wakeup.h"
 #include "../platform.h"
 
 int
 wakeup_launch (const appling_app_t *app, const char *url) {
-  return -1;
+  pid_t pid = fork();
+
+  if (pid < 0) return -1;
+
+  if (pid == 0) execlp(app->path, url, NULL);
+
+  return 0;
 }
